5f66c2666b1d59998466d7390ad3019cd67f36b7
Paul Grigoras <paul.grigoras90@gmail.com>
2014-05-16 03:15:17 +0100

    Add proper dispatcher test.

--- test/DispatcherTest.cpp
+++ test/DispatcherTest.cpp
@@ -4,42 +4,54 @@
 #include <iostream>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
+#include <cstring>
 
 using boost::asio::ip::tcp;
 using namespace std;
 
 int main() {
+  msg_t* msg = NULL;
+  int data_in[] = {1, 2, 3, 4};
+  int exp[] = {2, 3, 0, 0};
+
   try {
     boost::asio::io_service io_service;
     tcp::resolver resolver(io_service);
-    tcp::resolver::iterator endpoint_it = resolver.resolve(tcp::resolver::query("localhost", "8111"));
+    tcp::resolver::iterator endpoint_it = resolver.resolve(tcp::resolver::query("localhost", "8112"));
     tcp::socket socket(io_service);
     boost::asio::connect(socket, endpoint_it);
     for (;;) {
       char buf[1024];
       boost::system::error_code error;
 
-      int sizeBytes = sizeof(msg_t) + 2 * sizeof(int);
-      msg_t *msg = (msg_t *)calloc(sizeBytes, 1);
+      int sizeBytes = sizeof(msg_t) + 5 * sizeof(int);
+      msg = (msg_t *)calloc(sizeBytes, 1);
       msg->msgId = MSG_MOVING_AVG;
-      msg->dataSize = 1;
+      msg->dataSize = 4;
       msg->paramsSize = 1;
-      *(msg->data) = 13;
-      *(msg->data + sizeof(int)) = 2;
+      memcpy(msg->data, data_in, sizeof(int) * msg->dataSize);
+      *(msg->data + sizeof(int) * msg->dataSize) = 3;
 
       memcpy(buf, msg, sizeBytes);
 
       socket.write_some(boost::asio::buffer(buf), error);
-      size_t reply_len = socket.read_some(boost::asio::buffer(buf), error);
+
+      char buf2[1024];
+      size_t reply_len = socket.read_some(boost::asio::buffer(buf2), error);
+
+      msg = (msg_t*)buf2;
 
       if (error == boost::asio::error::eof)
         break; // Connection closed cleanly by peer.
       else if (error)
         throw boost::system::system_error(error); // Some other error.
 
-      cout.write(buf, reply_len);
     }
   } catch (exception& e) {
     cerr << e.what() << endl;
   }
+
+  msg->print();
+
+  return msg && (memcmp(exp, msg->data, 4 * sizeof(int)) == 0) ? 0 : 1;
 }
