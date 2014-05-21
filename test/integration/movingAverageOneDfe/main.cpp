be03865aae4004b2866aa1f8cef5fdc64f979f5f
Paul Grigoras <paul.grigoras90@gmail.com>
2014-05-21 01:37:05 +0100

    Make test size variable.

--- test/integration/movingAverageOneDfe/main.cpp
+++ test/integration/movingAverageOneDfe/main.cpp
@@ -8,18 +8,26 @@ using namespace std;
 
 int main() {
 
-  int data[] = {1, 2, 3, 4, 5, 6};
-  int exp[]  = {2, 3, 4, 5, 0, 0};
+  int n = 16;
+
+  int *data = (int *)calloc(n, sizeof(int));
+  for (int i = 0; i < n; i ++)
+    data[i] = i;
+
+  int *exp = (int *)calloc(n, sizeof(int));
+  for (int i = 1; i < n - 1; i ++)
+    exp[i] = (data[i - 1] + data[i] + data[i + 1]) / 3;
+
 
   /** Ask for a simple moving average a few times */
   int t = 1;
-  int out[6];
-  memset(out, 0, sizeof(int) * 6);
+  int out[n];
+  memset(out, 0, sizeof(int) * n);
 
   int status = 0;
   for (int k = 0; k < t; k++) {
-    movingAverage(6, 3, data, out);
-    for (int i = 0; i < 6; i++) {
+    movingAverage(n, 3, data, out);
+    for (int i = 1; i < n - 1; i++) {
       cout << "Checking data " << endl;
       if (out[i] != exp[i]) {
         cout << "Error " << i << " expected: " << exp[i] << " got: " << out[i];
