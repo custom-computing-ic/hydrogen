3c17781dba029e157314e8193650ff307e45e12c
Paul Grigoras <paul.grigoras90@gmail.com>
2014-05-21 16:04:51 +0100

    Fix MultiUserSingleJobType test on DFE.

--- test/integration/MultiUserSingleJobType.cpp
+++ test/integration/MultiUserSingleJobType.cpp
@@ -10,19 +10,27 @@ using namespace std;
 
 int main() {
 
-  int n = 10;
-  for (int i = 0; i < n; i++) {
+  int p = 10;
+  for (int i = 0; i < p; i++) {
     int pid = fork();
     if (pid == 0) {
-      int data[] = {1, 2, 3, 4, 5, 6};
-      int exp[]  = {2, 3, 4, 5, 0, 0};
 
-      int out[6];
-      memset(out, 0, sizeof(int) * 6);
+      int n = 16;
+
+      int *data = (int *)calloc(n, sizeof(int));
+      for (int i = 0; i < n; i ++)
+	data[i] = i;
+
+      int *exp = (int *)calloc(n, sizeof(int));
+      for (int i = 1; i < n - 1; i ++)
+	exp[i] = (data[i - 1] + data[i] + data[i + 1]) / 3;
+
+      int out[n];
+      memset(out, 0, sizeof(int) * n);
 
       int status = 0;
-      movingAverage(6, 3, data, out);
-      for (int i = 0; i < 6; i++) {
+      movingAverage(n, 3, data, out);
+      for (int i = 1; i < n - 1; i++) {
         if (out[i] != exp[i]) {
           cout << "Error " << i << " expected: " << exp[i] << " got: " << out[i];
           status = -1;
@@ -36,7 +44,7 @@ int main() {
   }
 
   // wait for all children to finish
-  for (int i = 0; i < n; i++ ) {
+  for (int i = 0; i < p; i++ ) {
     wait(NULL);
   }
 }
