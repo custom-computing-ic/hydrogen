52c08402ddd3f5de181d65bd8461f444ac5dafa1
Max Tottenham <mt3410@imperial.ac.uk>
2014-05-22 09:39:41 +0100

    fixed the startprocs.sh script to start the executor properly

--- scripts/startprocs.sh
+++ scripts/startprocs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-../build/dispatcher 8113 1> ./log/dispatcher.out  2> ./log/dispatcher.err&
+../build/dispatcher 8116 1> ./log/dispatcher.out  2> ./log/dispatcher.err&
 printf "Starting Dispatcher"
 sleep 1s
 printf "."
@@ -10,7 +10,7 @@ printf "."
 sleep 1s
 printf ".\n"
 
-../build/scheduler 8112 8113 1> ./log/scheduler.out  2> ./log/scheduler.err&
+../build/scheduler 8115 8116 1> ./log/scheduler.out  2> ./log/scheduler.err&
 printf "Starting Scheduler"
 sleep 1s
 printf "."
@@ -18,7 +18,7 @@ sleep 1s
 printf "."
 sleep 1s
 printf ".\n"
-../build/executor 8111 8112 1> ./log/executor.out  2> ./log/executor.err&
+../build/executor -p 8114 -s 8115 -n localhost 1> ./log/executor.out  2> ./log/executor.err&
 printf "Starting Executor"
 sleep 1s
 printf "."
