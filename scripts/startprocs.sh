963970693e81589733503c856215e7cc24b6730a
Max Tottenham <mt3410@imperial.ac.uk>
2014-05-22 12:42:13 +0100

    updated script to reflect executor changes

--- scripts/startprocs.sh
+++ scripts/startprocs.sh
@@ -18,7 +18,7 @@ sleep 1s
 printf "."
 sleep 1s
 printf ".\n"
-../build/executor -p 8114 -s 8115 -n localhost 1> ./log/executor.out  2> ./log/executor.err&
+../build/executor -c 4 -s 8115 -n localhost 1> ./log/executor.out  2> ./log/executor.err&
 printf "Starting Executor"
 sleep 1s
 printf "."
