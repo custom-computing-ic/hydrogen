f4d1bced3bdce004c9766272e53cae79e468ba86
Max Tottenham <mtottenh@gmail.com>
2014-05-27 09:38:01 +0100

    Added a resource type field

--- src/common/Resource.cpp
+++ src/common/Resource.cpp
@@ -1,15 +1,4 @@
 #include <Resource.hpp>
 /*
-void Resource::start() {
-  Client::start();
-}
-
-void Resource::getResult(void *out) {
-  Client::getResult(out);
-}
-
-void Resource::stop() {
-  Client::stop();
-i};
-
-*/
+ *
+ */
--- src/common/Resource.hpp
+++ src/common/Resource.hpp
@@ -2,7 +2,6 @@
 #define _RESOURCE_H_
 #include <Client.hpp>
 #include <iostream>
-//typedef std::deque<Resource> ResourcePool;
 
 
 class Resource {
@@ -12,13 +11,19 @@ class Resource {
         rid = r.getId();
         port = r.getPort();
         name = r.getName();
+        type = r.getType();
         /* should we copy idle/util time? */
     }
 
-    Resource(int port_, const std::string& name_, int id) 
+    Resource( int id, 
+              int port_, 
+              const std::string& name_, 
+              const std::string& type_) 
+
         : rid (id),
           port (port_),
-          name (name_) 
+          name (name_),
+          type (type_)
     {
       utilization = 0.0;
       idle_time = 0.0;
@@ -30,6 +35,7 @@ class Resource {
     int getId() const { return rid; }
     int getPort() const {return port;}
     const std::string& getName() const  {return name;}
+    const std::string& getType() const { return type;}
     friend std::ostream& operator<< (std::ostream& os, const Resource& r) {
       os << r.rid;
       return os;
@@ -47,6 +53,7 @@ class Resource {
     float utilization;
     float idle_time;
     std::string name;
+    std::string type;
 };
 
 #endif
