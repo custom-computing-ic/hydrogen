39e162d7ed7afcf7f950d3a6c03d4b44711cae39
Max Tottenham <mt3410@imperial.ac.uk>
2014-06-14 13:33:01 +0100

    Updated test scripts.

--- scripts/scenario1.sh
+++ scripts/scenario1.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-
+../scripts/killprocs.sh
+../scripts/startprocs.sh
+sleep 0.2
 echo "Running test: "
 for i in {1..2}
 do
   CLIENT_ID=4 ./MovingAverageOneDfe 1> mav.out 2> mav.err &
-  sleep 1
+  
 done
-
+echo -n "["
 for job in `jobs -p`
 do
-  echo $job
+  echo -n "."
   wait $job || let "FAIL+=1"
 done
+echo "]"
 
 CLIENT_ID=4 ./TerminateServer
--- scripts/startprocs.sh
+++ scripts/startprocs.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
 set -e
-./dispatcher 8112 1> /dev/null &
-printf "Starting Dispatcher"
-sleep 1s
+if [ -z "$MAXELEROSDIR" ]; then
+  printf "Starting Dispatcher Without DFE Implementations"
+  ./dispatcher 8112 1> /dev/null &
+else
+  printf "Starting Dispatcher With DFE Implementations"
+  LD_PRELOAD=/opt/maxeler/maxeleros/lib/libmaxeleros.so ./dispatcher 8112 1> /dev/null &
+fi
+
+sleep 0.2s
 printf "."
-sleep 1s
+sleep 0.2s
 printf "."
-sleep 1s
+sleep 0.2s
 printf ".\n"
 
 ../build/scheduler 8111 8112 2> ./log/scheduler.err | grep INFO &
 printf "Starting Scheduler"
-sleep 1s
+sleep 0.2s
 printf "."
-sleep 1s
+sleep 0.2s
 printf "."
-sleep 1s
+sleep 0.2s
 printf ".\n"
 ./executor -c 4 -p 8111 -n localhost  1> exe.out 2> exe.err &
 printf "Starting Executor"
-sleep 1s
+sleep 0.2s
 printf "."
-sleep 1s
+sleep 0.2s
 printf "."
-sleep 1s
+sleep 0.2s
 printf ".\n"
 printf "All Processes started. Ready for tests\n"
 
