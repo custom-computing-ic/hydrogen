0c5a38cb63df3f71d9df777c0b48e6c6cdf61c70
Paul Grigoras <paul.grigoras90@gmail.com>
2014-06-29 09:27:09 -0700

    Add dispatcher test group.

---
+++ test/dispatcher/DispatcherTest.cpp
@@ -0,0 +1,40 @@
+#include "TestLib.hpp"
+
+#include <Dispatcher.hpp>
+#include <Client.hpp>
+#include <message.hpp>
+
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <cstring>
+
+using boost::asio::ip::tcp;
+using namespace std;
+
+int main() {
+  msg_t* msg = NULL;
+  int n = mavg_data_size;
+  int *a = mavg_data(n);
+  int *out = (int *)calloc(n, sizeof(int));
+  int *exp = mavg_threept_exp(n, a);
+
+  try {
+    Client c(dispatcherPort, localhost);
+    c.start();
+
+    msg_t* msg = msg_moving_avg(n, 3, a);
+    char* buf = (char* )calloc(msg->totalBytes, 1);
+    memcpy(buf, msg, msg->totalBytes);
+    c.send(msg);
+    c.getResult(out, n * sizeof(int));
+    c.stop();
+    free(buf);
+  } catch (const exception& e) {
+    cerr << "Caught exception" << endl;
+    cerr << e.what() << endl;
+    return 1;
+  }
+
+  return mavg_check(n, out, exp)? 0 : 1;
+}
